70d0303e4f707db72893ab8451d28c2636fbdc2d
safwanvk <safwanvalakundil@gmail.com>
2020-08-20 14:31:09 +0530

    :zap: Create model Search

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=120)
+    created = models.DateTimeField(auto_created=True)
