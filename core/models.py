d658ffd3405ee80756590baad780e6143c594d0b
safwanvk <safwanvalakundil@gmail.com>
2020-08-20 16:23:40 +0530

    :lipstick: Create template base

--- core/models.py
+++ core/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=120)
     created = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.search
+
+    class Meta:
+        verbose_name_plural = 'searches'
+
