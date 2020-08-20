fd1c4709b03c1fdd80f42b850c2acc0586f01002
safwanvk <safwanvalakundil@gmail.com>
2020-08-20 22:55:30 +0530

    :white_check_mark: craigslist functionality

--- core/models.py
+++ core/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
-    search = models.CharField(max_length=120)
+    search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_created=True)
+
+
 
     def __str__(self):
         return self.search
