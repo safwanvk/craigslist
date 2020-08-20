0de210de63aef697bec4da6ce63ddfaafa53223e
safwanvk <safwanvalakundil@gmail.com>
2020-08-20 17:13:21 +0530

    :lipstick: Create template new_search

--- core/views.py
+++ core/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {
+        'search': search
+    }
+    return render(request, 'core/new_search.html', context)
